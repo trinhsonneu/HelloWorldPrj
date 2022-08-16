930df0459b14c2079fce130cbd6286ac5b7cd984
trinhsonneu <trinhsonneu@gmail.com>
2022-08-16 14:11:59 +0700

    Mấy cái này do thâyf Sơn sửa

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -56,7 +56,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(65, 142);
+            this.label2.Location = new System.Drawing.Point(195, 168);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(128, 20);
             this.label2.TabIndex = 2;
